c057e8cf9f0a61564e1ab692daa9e2dab63053b6
Roberto <Roonyi@gmail.com>
2020-05-23 13:50:27 -0500

    First Commit

--- Whatsapp_text.py
+++ Whatsapp_text.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 
 account_sid = 'AC6156524fb3848c6d808143475c74ff64'
-auth_token = 'e10eab24722d17b7fa1f144ea1e4fb4f'
+auth_token = '4bc1ec16743cfd126ad1a8ff739e4a90'
 client = Client(account_sid, auth_token)
 def send_message():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
-        body='Hola Anita!!! Llena por favor esta encuesta.. gracias',
+        body='Your appointment is coming up on July 21 at 3PM',
         to='whatsapp:+51920893272'
     )
 
